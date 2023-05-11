db2653696d67778840dae28bcb154311d25f4f83
Matthew Fallon <matthewfallon33@gmail.com>
2023-05-11 15:52:17 +0100

    tidy up in recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,6 @@
 
 const mongoose = require('mongoose');
 const fs = require('fs');
-const { log } = require('console');
 const { IMAGE_FOLDER } = process.env;
 
 const RecipeSchema = new mongoose.Schema({
@@ -37,51 +36,6 @@ const setSearchField = (field) => {
 const Recipe = mongoose.model("Recipes", RecipeSchema);
 module.exports = Recipe;
 
-module.exports.addRecipe = function (req) {
-
-    let recipe = new Recipe({
-        name: req.body.name,
-        duration: req.body.duration,
-        user: req.userId,
-        images: []
-    });
-
-    return recipe.save()
-}
-
-module.exports.addImages = function (id, images) {
-
-    return Recipe.findByIdAndUpdate(id,
-        { $push: { "images": { $each: images } } }
-        , { new: true })
-
-}
-
-module.exports.addImages = function (id, images) {
-
-    return Recipe.findByIdAndUpdate(id,
-        { $push: { "images": { $each: images } } }
-        , { new: true })
-
-}
-
-module.exports.removeImage = function (id, image) {
-
-    return Recipe.findByIdAndUpdate(id,
-        { $pull: { "images": image } }
-        , { new: true })
-
-}
-
-module.exports.updateRecipe = function (id, req) {
-
-    return Recipe.findByIdAndUpdate(id, {
-        "name": req.body.name,
-        "duration": req.body.duration
-    }, { new: true })
-
-}
-
 module.exports.getRecipes = function (req) {
 
     let query = {};
@@ -116,11 +70,48 @@ module.exports.getRecipe = function (id) {
 
 }
 
+module.exports.addRecipe = function (req) {
+
+    let recipe = new Recipe({
+        name: req.body.name,
+        duration: req.body.duration,
+        user: req.userId,
+        images: []
+    });
+
+    return recipe.save()
+}
+
+module.exports.updateRecipe = function (id, req) {
+
+    return Recipe.findByIdAndUpdate(id, {
+        "name": req.body.name,
+        "duration": req.body.duration
+    }, { new: true })
+
+}
+
 module.exports.removeRecipe = async function (id) {
 
     let recipe = await Recipe.findById(id);
     removeImages(recipe.images);
 
     return Recipe.findByIdAndDelete(id);
+
+}
+
+module.exports.addImages = function (id, images) {
+
+    return Recipe.findByIdAndUpdate(id,
+        { $push: { "images": { $each: images } } }
+        , { new: true })
+
+}
+
+module.exports.removeImage = function (id, image) {
+
+    return Recipe.findByIdAndUpdate(id,
+        { $pull: { "images": image } }
+        , { new: true })
 
 }
